4b3006f342213457c9f9a2311b1c0b6f73a86bd8
ricky <ricky_moore@ymail.com>
2024-02-24 16:03:57 -0800

    working on the header

---
+++ app/src/components/Header/index.js
@@ -0,0 +1,42 @@
+import "./Header.css";
+
+import me from './images/me.jpg'
+
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Header() {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  // Close the mobile menu when the screen size changes to desktop
+  const handleResize = () => {
+    if (window.innerWidth >= 768) {
+      setIsMobileMenuOpen(false);
+    }
+  };
+
+  // Add a resize event listener to handle the screen size change
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return (
+    <header className="header">
+      <div className="header-content">
+        <img className="header-img" src={me}></img>
+        <p className="header-name">Ricky Allen Moore Jr</p>
+        <i class="fa-solid fa-bars fa-2xl"></i>
+      </div>
+    </header>
+  )
+};
+
+export default Header;
